1d878a25827396dd02d03a5dd512e6633bd7b1d8
murali-mohan <muralimohan.sampath@outlook.com>
2025-06-23 13:27:43 -0400

    User Authentication functionalities

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -29,8 +29,9 @@ func (app *application) notFound(w http.ResponseWriter) {
 func (app *application) newTemplateData(r *http.Request) *templateData {
 
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -73,4 +74,8 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 		return err
 	}
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
